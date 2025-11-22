7f0c2ac600b9dd77d87add54eb85dbc3219b7ad1
akshaykarnavar <135056340+akshaykarnavar@users.noreply.github.com>
2025-11-23 03:28:39 +0530

    Add files via upload

---
+++ Assignment 30/program3.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+
+void pattern(int iRow,int iCol)
+{
+    int i=0,j=0;
+
+     if(iRow!=iCol)
+        {
+            printf("Input Invalid");
+            printf("Need irow and icol same");
+             return ;
+        }
+   
+    for(i=1;i<=iRow;i++)
+    {
+        
+        for(j=1;j<=iCol;j++)
+        {
+           
+            
+           if(j==i)
+            {
+                printf("$\t");
+                
+            }
+             
+           else
+            {
+               printf("*\t");
+            }
+            
+
+        
+            
+           
+        }
+       printf("\n");
+       
+    }
+    printf("\n");
+
+}
+
+int main()
+{
+
+    int iValue1=0,iValue2=0;
+
+    printf("Enter the Number of Rows:");
+    scanf("%d",&iValue1);
+
+    printf("Enter the Number of Columns:");
+    scanf("%d",&iValue2);
+
+    pattern(iValue1,iValue2);
+
+
+    return 0;
+}
