aed5cf76520067556d672b65de360fd22ede8586
akshaykarnavar <135056340+akshaykarnavar@users.noreply.github.com>
2025-11-23 03:26:07 +0530

    Add files via upload

---
+++ Assignment 28/program1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void pattern(int iRow,int iCol)
+{
+    int i=0,j=0;
+    
+    for(i=1;i<=iRow;i++)
+    {
+        char ch='A';
+        for(j=1;j<=iCol;j++)
+        {
+            printf("%c",ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+}
+
+int main()
+{
+
+    int iValue1=0,iValue2=0;
+
+    printf("Enter the Number of Rows:");
+    scanf("%d",&iValue1);
+
+    printf("Enter the Number of Columns:");
+    scanf("%d",&iValue2);
+
+    pattern(iValue1,iValue2);
+
+
+    return 0;
+}
---
+++ Assignment 28/program2.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+void pattern(int iRow,int iCol)
+{
+    int i=0,j=0;
+    
+    for(i=1;i<=iRow;i++)
+    {
+        if(i%2==0)
+        {
+           char ch='a';
+
+        for(j=1;j<=iCol;j++)
+        {
+        
+            printf("%c",ch);
+            ch++;
+        }
+        }
+        else
+        {
+            char ch='A';
+
+        for(j=1;j<=iCol;j++)
+        {
+        
+            printf("%c",ch);
+            ch++;
+        }
+        }
+       
+
+        printf("\n");
+    }
+    printf("\n");
+
+}
+
+int main()
+{
+
+    int iValue1=0,iValue2=0;
+
+    printf("Enter the Number of Rows:");
+    scanf("%d",&iValue1);
+
+    printf("Enter the Number of Columns:");
+    scanf("%d",&iValue2);
+
+    pattern(iValue1,iValue2);
+
+
+    return 0;
+}
