f26cb91cc812431e379bef821cdc780f8b115a14
akshaykarnavar <135056340+akshaykarnavar@users.noreply.github.com>
2025-11-25 14:47:14 +0530

    Add files via upload

---
+++ Assignment 32/program3.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+class Pattern
+{
+    public void Display(int iRow,int iCol)
+    {
+        int i,j=0;
+
+            if(iRow!=iCol)
+        {
+            System.out.println("Input Invalid");
+             System.out.println("Need irow and icol same");
+             return ;
+        }
+         
+        for(i=1;i<=iRow;i++)
+        {
+           for(j=iCol;j>=1;j--)
+           {
+            if(i==j||i==1||i==iRow||j==1||j==iCol)
+            {
+             System.out.print("*\t");
+            }
+        
+             else
+            {
+                 System.out.print(" \t");
+            }
+          
+            
+           }
+           System.out.println();
+            
+            
+        
+        }
+    }
+
+}
+
+class program3
+{
+    public static void main(String A[])
+    {
+        int iValue1=0,iValue2=0;
+
+        Scanner sobj=new Scanner(System.in);
+
+        System.out.println("Enter No of Rows:");
+        iValue1=sobj.nextInt();
+
+        System.out.println("Enter No of Columns:");
+        iValue2=sobj.nextInt();
+
+        Pattern pobj=new Pattern();
+        pobj.Display(iValue1,iValue2);
+
+
+
+
+    }
+}
